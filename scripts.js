8182d5c84d365da40e04cdb3f940499fc878936f
Jan <kankovskyj@email.cz>
2020-07-03 20:13:29 +0200

    dropzones, fuel works

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-//$('input.cb-value').prop("checked", true);
+//nav active/inactive toggle
 $('.cb-value').click(function() {
     var mainParent = $(this).parent('.toggle-btn');
     if($(mainParent).find('input.cb-value').is(':checked')) {
@@ -9,7 +9,18 @@ $('.cb-value').click(function() {
       $("#active-state").text("Neaktivní");
     }
   });
-
+//disable extended opening hours
   $('#styled-checkbox-nonstop').click(function(){
     $('.day').toggleClass("disable");
   });
+
+//add fuel
+  $("#add-fuel").click(function () {
+    $("#fuel-container").append('<section class="fuel-row"><div class="fuel-column"><h6>Název</h6><select class="form-control form-control-lg"><option>Vyberte...</option><option>Diesel</option><option>Benzin</option></select></div><div class="fuel-column"><h6>Číslo</h6><input class="form-control form-control-lg" type="tel" placeholder=""></div><div class="delete-column"><h6>Smazat</h6><div class="delete-img" id="delete-fuel"></div></div></section>');
+  });
+//remove fuel
+$("body").on("click", ".delete-img", function() {
+  $(this).closest('section').remove();
+});
+
+ 
